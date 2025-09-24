eee382e08ac18328f7ff7f2872fecd2a9c5265a4
unknown <arthurluiz.developer@gmail.com>
2025-09-24 10:40:25 +0100

    Exercicio 2 e 3

---
+++ CuteAnimal/CatType.cs
@@ -0,0 +1,12 @@
+namespace CuteAnimal
+{
+    public enum CatType
+    {
+        Persian,
+        Siamese,
+        MaineCoon,
+        Bengal,
+        ScottishFold
+
+    }
+}
---
+++ CuteAnimal/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Numerics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+
+namespace CuteAnimal
+{
+    public class Program
+    {
+        private CatType type;
+        private Feed hungry;
+        private Mood mood;
+        private Vector3 dimensions;
+        private string name;
+        private float weight;
+        private int age;
+
+        private static void Main(string[] args)
+        {
+
+        }
+
+        private void MakeSound()
+        {
+            
+            Random randNumber = new Random();
+            int a = randNumber.Next(0, 3);
+
+            switch (a)
+            {
+                case 0:
+                    Console.WriteLine("Meow!");
+                    break;
+                case 1:
+                    Console.WriteLine("purr!");
+                    break;
+                case 2:
+                    Console.WriteLine("grr!");
+                    break;
+            }
+        }
+    }
+}
